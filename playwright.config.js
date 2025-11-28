2f7662ec9f9510120cc69ea79c3bc35e81e1f3b9
Sakshi Agarwal <agarwalsakshi004@gmail.com>
2025-11-29 01:59:31 +0530

    sum

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 reporter: [
   ['list'],
-  ['json', { outputFile: 'test-results/report.json' }],
+  
   ['html', { outputFolder: 'playwright-report', open: 'never' }]
 ],
 
